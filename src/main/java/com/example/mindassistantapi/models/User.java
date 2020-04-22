7e6cd0f64fe326b06457819f4871a69562a1982e
okapetanios <ericegan@live.com>
2020-04-21 23:27:25 -0700

    back to not jsonignore

--- src/main/java/com/example/mindassistantapi/models/User.java
+++ src/main/java/com/example/mindassistantapi/models/User.java
@@ -1,5 +1,6 @@
 package com.example.mindassistantapi.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.util.List;
@@ -47,6 +48,7 @@ public class User {
     private com.example.mindassistantapi.models.Profile profile;
 
     @OneToMany(mappedBy = "user")
+    @JsonBackReference
     private List<com.example.mindassistantapi.models.Note> notes;
 
     @OneToMany(mappedBy = "user")
